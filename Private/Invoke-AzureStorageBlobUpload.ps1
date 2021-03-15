a7f330956f36408c3f763988f160acc9a6e63779
Nickolaj Andersen <nickolaj@outlook.com>
2021-03-15 07:41:50 +0100

    1.0.2

--- Private/Invoke-AzureStorageBlobUpload.ps1
+++ Private/Invoke-AzureStorageBlobUpload.ps1
@@ -13,11 +13,12 @@ function Invoke-AzureStorageBlobUpload {
         Author:      Nickolaj Andersen
         Contact:     @NickolajA
         Created:     2020-01-04
-        Updated:     2020-09-20
+        Updated:     2021-03-15
 
         Version history:
         1.0.0 - (2020-01-04) Function created
         1.0.1 - (2020-09-20) Fixed an issue where the System.IO.BinaryReader wouldn't open a file path containing whitespaces
+        1.0.2 - (2021-03-15) Fixed an issue where SAS Uri renewal wasn't working correctly
     #>    
     param(
         [parameter(Mandatory = $true)]
@@ -41,27 +42,36 @@ function Invoke-AzureStorageBlobUpload {
     $FileSize = (Get-Item -Path $FilePath).Length
     $ChunkCount = [System.Math]::Ceiling($FileSize / $ChunkSizeInBytes)
     $BinaryReader = New-Object -TypeName System.IO.BinaryReader([System.IO.File]::Open($FilePath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::ReadWrite))
-
     $Position = $BinaryReader.BaseStream.Seek(0, [System.IO.SeekOrigin]::Begin)
 
-    # Upload each chunk. Check whether a SAS URI renewal is required after each chunk is uploaded and renew if needed
+    # Upload each chunk and dheck whether a SAS URI renewal is required after each chunk is uploaded and renew if needed
     $ChunkIDs = @()
     for ($Chunk = 0; $Chunk -lt $ChunkCount; $Chunk++) {
+        Write-Verbose -Message "SAS Uri renewal timer has elapsed: $($SASRenewalTimer.Elapsed.Minutes) minute $($SASRenewalTimer.Elapsed.Seconds) seconds"
+
+        # Convert and calculate required chunk elements for content upload
         $ChunkID = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($Chunk.ToString("0000")))
         $ChunkIDs += $ChunkID
         $Start = $Chunk * $ChunkSizeInBytes
         $Length = [System.Math]::Min($ChunkSizeInBytes, $FileSize - $Start)
         $Bytes = $BinaryReader.ReadBytes($Length)
+
+        # Increment chunk to get the current chunk
         $CurrentChunk = $Chunk + 1
 
         Write-Progress -Activity "Uploading file to Azure Storage blob" -Status "Uploading chunk $($CurrentChunk) of $($ChunkCount)" -PercentComplete ($CurrentChunk / $ChunkCount * 100)
         Write-Verbose -Message "Uploading file to Azure Storage blob, processing chunk '$($CurrentChunk)' of '$($ChunkCount)'"
         $UploadResponse = Invoke-AzureStorageBlobUploadChunk -StorageUri $StorageUri -ChunkID $ChunkID -Bytes $Bytes
+        
         if (($CurrentChunk -lt $ChunkCount) -and ($SASRenewalTimer.ElapsedMilliseconds -ge 450000)) {
-            Invoke-AzureStorageBlobUploadRenew -Resource $Resource
+            Write-Verbose -Message "SAS Uri renewal is required, attempting to renew"
+            $RenewedSASUri = Invoke-AzureStorageBlobUploadRenew -Resource $Resource
             $SASRenewalTimer.Restart()
         }
     }
+
+    # Stop timer
+    $SASRenewalTimer.Stop()
 
     # Complete write status progress bar
     Write-Progress -Completed -Activity "Uploading File to Azure Storage blob"
