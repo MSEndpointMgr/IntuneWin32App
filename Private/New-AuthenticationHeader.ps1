26c8ed5737c1188b11b6393183519572f2356d53
Nickolaj Andersen <nickolaj@outlook.com>
2021-09-08 20:39:59 +0200

    1.0.1

--- Private/New-AuthenticationHeader.ps1
+++ Private/New-AuthenticationHeader.ps1
@@ -13,10 +13,11 @@ function New-AuthenticationHeader {
         Author:      Nickolaj Andersen
         Contact:     @NickolajA
         Created:     2021-04-08
-        Updated:     2021-04-08
+        Updated:     2021-09-08
 
         Version history:
         1.0.0 - (2021-04-08) Script created
+        1.0.1 - (2021-09-08) Fixed issue reported by Paul DeArment Jr where the local date time set for ExpiresOn should be UTC to not cause any time related issues
     #>
     param(
         [parameter(Mandatory = $true, HelpMessage = "Pass the AuthenticationResult object returned from Get-AccessToken cmdlet.")]
