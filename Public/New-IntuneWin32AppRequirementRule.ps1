c898faacaf7f1d660ec3ecf178b779c7f8505669
Nickolaj Andersen <nickolaj@outlook.com>
2023-04-26 22:29:18 +0200

    1.4.1

--- Public/New-IntuneWin32AppRequirementRule.ps1
+++ Public/New-IntuneWin32AppRequirementRule.ps1
@@ -28,7 +28,7 @@ function New-IntuneWin32AppRequirementRule {
         Author:      Nickolaj Andersen
         Contact:     @NickolajA
         Created:     2020-01-27
-        Updated:     2022-10-02
+        Updated:     2023-04-26
 
         Version history:
         1.0.0 - (2020-01-27) Function created
@@ -36,6 +36,7 @@ function New-IntuneWin32AppRequirementRule {
         1.0.2 - (2021-08-31) Added new minimum supported operating system versions to parameter validation
         1.0.3 - (2022-09-02) minimumSupportedOperatingSystem property is replaced by minimumSupportedWindowsRelease
         1.0.4 - (2022-10-02) minimumFreeDiskSpaceInMB, MinimumMemoryInMB, MinimumNumberOfProcessors and minimumCpuSpeedInMHz now adds a 'null' string
+        1.0.5 - (2023-04-26) Added support for new Windows 10 and Windows 11 minimum operating system versions
     #>    
     [CmdletBinding(SupportsShouldProcess = $true)]
     param(
@@ -46,7 +47,7 @@ function New-IntuneWin32AppRequirementRule {
 
         [parameter(Mandatory = $true, HelpMessage = "Specify the minimum supported Windows release version as a requirement for the Win32 app.")]
         [ValidateNotNullOrEmpty()]
-        [ValidateSet("1607", "1703", "1709", "1803", "1809", "1903", "1909", "2004", "20H2", "21H1")]
+        [ValidateSet("W10_1607", "W10_1703", "W10_1709", "W10_1803", "W10_1809", "W10_1903", "W10_1909", "W10_2004", "W10_20H2", "W10_21H1", "W10_21H2", "W10_22H2", "W11_21H2", "W11_22H2")]
         [string]$MinimumSupportedWindowsRelease,
 
         [parameter(Mandatory = $false, HelpMessage = "Specify the minimum free disk space in MB as a requirement for the Win32 app.")]
@@ -75,16 +76,20 @@ function New-IntuneWin32AppRequirementRule {
 
         # Construct table for supported operating systems
         $OperatingSystemTable = @{
-            "1607" = "1607"
-            "1703" = "1703"
-            "1709" = "1709"
-            "1803" = "1803"
-            "1809" = "1809"
-            "1903" = "1903"
-            "1909" = "1909"
-            "2004" = "2004"
-            "20H2" = "2H20"
-            "21H1" = "21H1"
+            "W10_1607" = "1607"
+            "W10_1703" = "1703"
+            "W10_1709" = "1709"
+            "W10_1803" = "1803"
+            "W10_1809" = "1809"
+            "W10_1903" = "1903"
+            "W10_1909" = "1909"
+            "W10_2004" = "2004"
+            "W10_20H2" = "2H20"
+            "W10_21H1" = "21H1"
+            "W10_21H2" = "Windows10_21H2"
+            "W10_22H2" = "Windows10_22H2"
+            "W11_21H2" = "Windows11_21H2"
+            "W11_22H2" = "Windows11_22H2"
         }
 
         # Construct ordered hash-table with least amount of required properties for default requirement rule
