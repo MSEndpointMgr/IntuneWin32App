66886b4bc2620566d433d84725bb97f4662f9227
Jasper Metselaar <jms@du.se>
2023-12-22 14:05:09 +0100

    Added detection and requirement rules to the Set-IntuneWin32Apps.ps1

--- Public/Set-IntuneWin32App.ps1
+++ Public/Set-IntuneWin32App.ps1
@@ -39,6 +39,16 @@ function Set-IntuneWin32App {
     .PARAMETER CompanyPortalFeaturedApp
         Specify whether to have the Win32 application featured in Company Portal or not.
 
+    .PARAMETER DetectionRule
+        Provide an array of a single or multiple OrderedDictionary objects as detection rules that will be used for the Win32 application.
+    
+    .PARAMETER RequirementRule
+        Provide an OrderedDictionary object as requirement rule that will be used for the Win32 application.
+
+    .PARAMETER AdditionalRequirementRule
+        Provide an array of OrderedDictionary objects as additional requirement rule, e.g. for file, registry or script rules, that will be used for the Win32 application.
+
+
     .NOTES
         Author:      Nickolaj Andersen
         Contact:     @NickolajA
@@ -96,7 +106,19 @@ function Set-IntuneWin32App {
         [bool]$CompanyPortalFeaturedApp,
 
         [parameter(Mandatory = $false, HelpMessage = "Specify whether to allow the Win32 application to be uninstalled from the Company Portal app when assigned as available.")]
-        [bool]$AllowAvailableUninstall
+        [bool]$AllowAvailableUninstall,
+
+        [parameter(Mandatory = $false, HelpMessage = "Provide an array of a single or multiple OrderedDictionary objects as detection rules that will be used for the Win32 application.")]
+        [ValidateNotNullOrEmpty()]
+        [System.Collections.Specialized.OrderedDictionary[]]$DetectionRule,
+
+        [parameter(Mandatory = $false, HelpMessage = "Provide an OrderedDictionary object as requirement rule that will be used for the Win32 application.")]
+        [ValidateNotNullOrEmpty()]
+        [System.Collections.Specialized.OrderedDictionary]$RequirementRule,
+
+        [parameter(Mandatory = $false, HelpMessage = "Provide an array of OrderedDictionary objects as additional requirement rule, e.g. for file, registry or script rules, that will be used for the Win32 application.")]
+        [ValidateNotNullOrEmpty()]
+        [System.Collections.Specialized.OrderedDictionary[]]$AdditionalRequirementRule
     )
     Begin {
         # Ensure required authentication header variable exists
@@ -157,6 +179,24 @@ function Set-IntuneWin32App {
             }
             if ($PSBoundParameters["AllowAvailableUninstall"]) {
                 $Win32AppBody.Add("allowAvailableUninstall", $AllowAvailableUninstall)
+            }
+            if ($PSBoundParameters["DetectionRule"]) {
+                # Validate that correct detection rules have been passed on command line, only 1 PowerShell script based detection rule is allowed
+                if (($DetectionRule.'@odata.type' -contains "#microsoft.graph.win32LobAppPowerShellScriptDetection") -and (@($DetectionRules).'@odata.type'.Count -gt 1)) {
+                    Write-Warning -Message "Multiple PowerShell Script detection rules were detected, this is not a supported configuration"; break
+                }
+               
+                # Add detection rules to Win32 app body object
+                Write-Verbose -Message "Detection rule objects passed validation checks, attempting to add to existing Win32 app body"
+                $Win32AppBody.Add("detectionRules", $DetectionRule)
+            }
+
+            if ($PSBoundParameters["RequirementRule"]) {
+                $Win32AppBody.Add("RequirementRule", $RequirementRule)
+            }
+
+            if ($PSBoundParameters["AdditionalRequirementRule"]) {
+                $Win32AppBody.Add("requirementRules", $AdditionalRequirementRule)
             }
 
             try {
