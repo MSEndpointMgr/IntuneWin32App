ea9ca2c89ac8cbe9909dacc40428c28099a78ee8
alexhass <4450722+alexhass@users.noreply.github.com>
2026-06-03 17:42:48 +0200

    Refactor authentication checks in IntuneWin32App requirement functions

--- Public/Remove-IntuneWin32AppRequirement.ps1
+++ Public/Remove-IntuneWin32AppRequirement.ps1
@@ -43,13 +43,8 @@ function Remove-IntuneWin32AppRequirement {
     )
     Begin {
         # Ensure required authentication header variable exists
-        if ($Global:AuthenticationHeader -eq $null) {
+        if (-not (Test-AuthenticationState)) {
             Write-Warning -Message "Authentication token was not found, use Connect-MSIntuneGraph before using this function"; break
-        }
-        else {
-            if ((Test-AccessToken) -eq $false) {
-                Write-Warning -Message "Existing token found but has expired, use Connect-MSIntuneGraph to request a new authentication token"; break
-            }
         }
 
         # Set script variable for error action preference
